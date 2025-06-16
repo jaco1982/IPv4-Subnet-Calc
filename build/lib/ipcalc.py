eb2dc4f32caf1bfd15b0a10bf5ac8303c5d8c04d
Jaco du Plessis <5507425+jaco1982@users.noreply.github.com>
2025-06-16 19:58:36 +0200

    Updates to make installable

---
+++ build/lib/ipcalc.py
@@ -0,0 +1,26 @@
+import json, sys
+from subnet_calc.wrapper import calc_subnet
+
+def main():
+    address = ''
+    argument_list = sys.argv[1:] # Retrieve arguments
+
+    if len(argument_list) != 0:
+        address = argument_list[0]
+
+    if address == '': # If no CLI address is specified
+        address = input('Enter an IP Address in CIDR notation: ') 
+        if address == '': # If no address is specified
+            print('Address cannot be blank')
+
+    raw_results = calc_subnet(address)
+    results = json.loads(raw_results)
+
+    if results['output']['result']:
+        for key, value in results.items():
+            print(f'{key}:')
+            for sub_key, sub_value in value.items():
+                print(f'   {sub_key:<12}:  {sub_value}')
+            print()
+    else:
+        print('Error calculating results')
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='subnet_calc',
     version='0.1',
     packages=find_packages(),
+    py_modules=['ipcalc'],  # <-- add this line
     install_requires=[],
     entry_points={
         'console_scripts': [
